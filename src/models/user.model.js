14f7b8ab01ad682fc9a1307bd4e77cb9719c3276
puja-debnath <pujadebnath570@gmail.com>
2024-07-30 16:35:40 +0530

    model and tokens

---
+++ src/models/user.model.js
@@ -0,0 +1,98 @@
+import mongoose , { Schema} from "mongoose"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
+const UserSchema = new Schema({
+     username:{
+        type: String,
+        require:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+        index:true, //to make it more seachable in a optimized way
+     },
+     email:{
+        type: String,
+        require:true,
+        unique:true,
+        lowercase:true,
+        trim:true
+    },
+    fullName:{
+        type: String,
+        require:true,
+        trim:true,
+        index:true
+    },
+    avater:{
+        type:String, //clooudinary url
+        required:true
+    },
+    coverImage:{
+        type:String // cloudinary url
+    },
+    watchHistory:{
+        type:Schema.Types.ObjectId,
+        ref:"Video"
+    },
+    password:{
+        type:String ,  // try to encrypt data gets leak
+        required:[true,"password is required"]
+    },
+    refreshToken:{
+        type:String
+    }
+    
+},
+{
+    timestamps:true
+}
+)  
+
+
+// here dont use arrow callback cause  there is no this reference here, but we need the context here 
+//its a pre hook
+UserSchema.pre("save", async function(next) {
+    //if we dont use if then with every change it will update the password everytime
+    if(!this.isModified("password")) return next()
+      this.password = bcrypt.hash(this.password,10)
+    next()
+})
+
+ //now since the password is encrypted we have to check the password is correct or not 
+ UserSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password,this.password)
+}
+
+//we using both session and cookies for security 
+
+//access and refresh are both JWT tokens
+UserSchema.methods.generateAccessToken = function(){
+    //add the payload for tokens
+  return jwt.sign({
+     _id : this._id,
+     email:this.email,
+     username:this.username,
+     fullName: this.fullName
+   },
+   process.env.ACCESS_TOKEN_SECRET,
+   //access-token-expiry goes indie a object
+   {
+    expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+   }
+)
+}
+
+UserSchema.methods.generateRefreshToken = function(){
+    return jwt.sign({
+        _id : this._id,
+      },
+      process.env.REFRESH_TOKEN_SECRET,
+      {
+       expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+      }
+    )   
+}
+
+
+export const User = mongoose.model("User", UserSchema)
---
+++ src/models/video.model.js
@@ -0,0 +1,47 @@
+import mongoose , { Schema} from "mongoose"
+import mongoAggrigatePerinate from "mongoose-aggregate-paginate-v2"
+const VideoSchema = new Schema({
+  
+    VideoFile:{
+        type:String, //clooudinary url
+        required:true
+    },
+    thumbnail:{
+        type:String, // cloudinary url
+        required:true
+
+    },
+    title:{
+    type:String,
+    requird:true,
+
+   },
+    description:{
+    type:String,
+    requird:true,
+   },
+    duration:{
+    type:Number,
+    required:true
+   },
+    views:{
+        type:Number,
+        default: 0
+    },
+    isPublished:{
+        type:Boolean,
+        default:true
+    },
+    owner:{
+        type:Schema.Types.ObjectId,
+        red:"User"
+    }
+    
+},
+{
+    timestamps:true
+}
+)  
+
+ VideoSchema.plugin(mongoAggrigatePerinate)
+export const Video = mongoose.model("Video", VideoSchema)
